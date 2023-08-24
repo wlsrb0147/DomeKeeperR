6d635ddeb9300aa4e08eac93154bbcd3ffaa2b8f
21aje <diding0222@naver.com>
2023-08-24 19:41:03 +0900

    1

--- Assets/Wonjae_Folder/Scripts/PetController.cs
+++ Assets/Wonjae_Folder/Scripts/PetController.cs
@@ -1,101 +1,36 @@
 using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PetController : PetEntity
 {
-    [Header("Pet Info")]
-    [SerializeField] private float petSpeed;
-    [SerializeField] private float jumpForce;
-    [SerializeField] float P_attack;
-    [SerializeField] bool isMoving;
-
-    public S_Mineral mineral;
-
-    GameObject[] targetMineral;
-    Transform[] MineralTrs;
-    float[] PetAndMineralDist;
-
-    private Transform PetTr;
-    private Transform targetTr;
-    private int targetint = 0;
-
-    private NavMeshAgent nvAgent;
-
 
     protected override void Start()
     {
         base.Start();
-        mineral = GetComponent<S_Mineral>();
-        targetMineral = GameObject.FindGameObjectsWithTag("MIneral");
-        MineralTrs = new Transform[targetMineral.Length];
-
-        for (int i = 0; i < targetMineral.Length; i++)
-        {
-            MineralTrs[i] = targetMineral[i].GetComponent<Transform>();
-        }
-        targetTr = MineralTrs[0];
-        StartCoroutine(SearchTarget());
-    }
-
-    IEnumerator SearchTarget()
-    {
-        while (true)
-        {
-            yield return new WaitForSeconds(0.2f);
-            for (int i = 0; i < targetMineral.Length; i++)
-            {
-                if (PetAndMineralDist[targetint] <= PetAndMineralDist[i + 1])
-                {
-                    targetTr = MineralTrs[targetint];
-                }
-            }
-        }
-        nvAgent.destination = targetTr.position;
-        targetint = 0;
     }
 
     protected override void Update()
     {
         base.Update();
 
-
-
-    }
-
-
-
-    void PetUnderMine()
-    {
-        Collider2D groundCollider = Physics2D.OverlapCircle(footPos.position, 0.4f, whatIsGround);
-        Collider2D mineralCollider = Physics2D.OverlapCircle(footPos.position, 0.4f, WhatIsMineral);
-
-        if (groundCollider != null)
+        if (isGrounded)
         {
-            groundCollider.transform.GetComponent<S_MapGenerator>().MakeDot(footPos.position);
+            MoveVelocity();
+            sideMine = true;
+            underMine = false;
         }
-        else if (mineralCollider != null)
+
+        if (isMineraled)
         {
-            mineralCollider.transform.GetComponent<S_Mineral>().SetDamage(P_attack);
+            ZeroVelocity();
+            sideMine = false;
+            underMine = true;
         }
 
     }
 
 
-    void PetSideMine()
-    {
-        Collider2D groundCollider = Physics2D.OverlapCircle(toothPos.position, 0.5f, whatIsGround);
-        Collider2D mineralCollider = Physics2D.OverlapCircle(toothPos.position, 0.5f, WhatIsMineral);
-
-        if (groundCollider != null)
-        {
-            groundCollider.transform.GetComponent<S_MapGenerator>().MakeDot(toothPos.position);
-        }
-        else if (mineralCollider != null)
-        {
-            mineralCollider.transform.GetComponent<S_Mineral>().SetDamage(P_attack);
-        }
-
-    }
 }
--- Assets/Wonjae_Folder/Scripts/PetEntity.cs
+++ Assets/Wonjae_Folder/Scripts/PetEntity.cs
@@ -9,68 +9,151 @@ public class PetEntity : MonoBehaviour
     protected Animator anim;
     protected SpriteRenderer spr;
 
+    [Header("Pet Info")]
+    [SerializeField] private float petSpeed;
+    [SerializeField] private float petDamage;
+    [SerializeField] private float mineSpeed;
+
+
     [Header("Collision Info")]
     [SerializeField] protected Transform groundCheck;
     [SerializeField] protected private float groundCheckDistance;
-    //[SerializeField] protected Transform wallCheck;
-    //[SerializeField] protected private float wallCheckDistance;
-    [SerializeField] protected Transform mineralCheck;
+    [SerializeField] protected Transform mineralUnderCheck;
     [SerializeField] protected float mineralCheckDistance;
-    [SerializeField] protected Transform mineralSideCheck;
-    [SerializeField] protected float mineralSideCheckDistance;
     [SerializeField] protected private LayerMask whatIsGround;
-    //[SerializeField] protected private LayerMask whatIsWall;
     [SerializeField] protected private LayerMask WhatIsMineral;
     [SerializeField] protected Transform footPos;
     [SerializeField] protected Transform toothPos;
 
+
+    #region anim
     protected bool isGrounded;
-    //protected bool isWallDetected;
     protected bool isMineraled;
-    protected bool isSideMineraled;
+    protected bool petMove;
+    protected bool underMine;
+    protected bool sideMine;
+    protected bool petIdle;
+    #endregion
 
     protected int facingDir = -1;
     protected bool facingRight = true;
+
+    S_Mineral mineral;
+    bool mineralCheck;
 
     protected virtual void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spr = GetComponent<SpriteRenderer>();
+        mineral = GetComponent<S_Mineral>();
     }
 
     protected virtual void Update()
     {
         CollisionChecks();
+        PetAnimatorControllers();
     }
 
+    #region Flip 
     protected virtual void Flip()
     {
-        facingDir = facingDir * -1;
+        facingDir = facingDir* -1;
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
     }
+    private void FlipController()
+    {
+        if (rbody.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rbody.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
 
+    #endregion
+
+
+    #region CollisionChecks
     protected virtual void CollisionChecks()
     {
+
         isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
-        isMineraled = Physics2D.Raycast(mineralCheck.position, Vector2.down, mineralCheckDistance, WhatIsMineral);
-        isSideMineraled = Physics2D.Raycast(mineralCheck.position, Vector2.left, mineralCheckDistance, WhatIsMineral) 
-            || Physics2D.Raycast(mineralCheck.position, Vector2.right, mineralCheckDistance, WhatIsMineral);
+        isMineraled = Physics2D.Raycast(mineralUnderCheck.position, Vector2.down, mineralCheckDistance, WhatIsMineral);
     }
 
     protected virtual void OnDrawGizmos()
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
-        Gizmos.DrawLine(mineralCheck.position, new Vector3(mineralCheck.position.x, mineralCheck.position.y - mineralCheckDistance));
-        Gizmos.DrawLine(mineralSideCheck.position, new Vector3(mineralSideCheck.position.x + mineralSideCheckDistance * facingDir, mineralSideCheck.position.y));
-        Gizmos.DrawLine(mineralSideCheck.position, new Vector3(mineralSideCheck.position.x - mineralSideCheckDistance * facingDir, mineralSideCheck.position.y));
+        Gizmos.DrawLine(mineralUnderCheck.position, new Vector3(mineralUnderCheck.position.x, mineralUnderCheck.position.y - mineralCheckDistance));
     }
+    #endregion
+
+
     #region Velocity
 
     public void ZeroVelocity() => rbody.velocity = Vector2.zero;
+    public void MoveVelocity() => rbody.velocity = new Vector2(petSpeed * facingDir, rbody.velocity.y);
 
     #endregion
 
+
+    //private void petMovement()
+    //{
+    //   MoveVelocity();
+    //}
+
+    private void PetAnimatorControllers()
+    {
+        //bool isMoving = rbody.velocity.x != 0;
+        anim.SetBool("Pet_Move", petMove);
+        anim.SetBool("Pet_idle", petIdle);
+        anim.SetBool("Under_Mine", underMine);
+        anim.SetBool("Side_Mine", sideMine);
+
+    }
+
+    void PetUnderMine()
+    {
+        Collider2D groundCollider2d = Physics2D.OverlapCircle(footPos.position, 0.05f, whatIsGround);
+        Collider2D mineralCollider2d = Physics2D.OverlapCircle(footPos.position, 0.05f, WhatIsMineral);
+
+        if (mineralCollider2d != null)
+        {
+            mineralCollider2d.transform.GetComponent<S_Mineral>().SetDamage(petDamage);
+        }
+        else if (groundCollider2d != null && !mineralCheck)
+        {
+            groundCollider2d.transform.GetComponent<S_MapGenerator>().MakeDot(footPos.position);
+        }
+    }
+
+    void PetSideMine()
+    {
+        Collider2D groundCollider2d = Physics2D.OverlapCircle(toothPos.position, 0.01f, whatIsGround);
+        Collider2D mineralCollider2d = Physics2D.OverlapCircle(toothPos.position, 0.01f, WhatIsMineral);
+
+        if (mineralCollider2d != null)
+        {
+            mineralCollider2d.transform.GetComponent<S_Mineral>().SetDamage(petDamage);
+        }
+        else if (groundCollider2d != null && !mineralCheck)
+        {
+            groundCollider2d.transform.GetComponent<S_MapGenerator>().MakeDot(toothPos.position);
+        }
+    }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Mineral"))
+    //    {
+    //        mineralCheck = true;
+    //    }
+    //    else
+    //        mineralCheck = false;
+    //}
 
 }
