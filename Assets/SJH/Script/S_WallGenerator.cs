26b4913e386860bddf965ffb992dfacc1c3ed52b
poiz1245 <wva960@gmail.com>
2023-08-29 15:08:10 +0900

    1
    1

--- Assets/SJH/Script/S_WallGenerator.cs
+++ Assets/SJH/Script/S_WallGenerator.cs
@@ -22,13 +22,17 @@ public class S_WallGenerator : MonoBehaviour
     {
         int count = 0;
 
-        for (int j = 0; j > -mapSize.y; j--)
+        for (int j = mapSize.y / 2 - 1; j > -mapSize.y; j--)
         {
+            for (int i = mapSize.x / 2 + 1; i < mapSize.x / 2 + 4; i++)
+            {
+                tileMap.SetTile(new Vector3Int(i, j - 1, 0), wallTile);
+                tileMap.SetTile(new Vector3Int(-i, j - 1, 0), wallTile);
+            }
             for (int i = count; i < mapSize.x / 2; i++)
             {
-                //tileMap.SetTile(new Vector3Int(1,-50), wallTile);
-                tileMap.SetTile(new Vector3Int(i+1, j + mapSize.y / 2 - 1, 0), wallTile);
-                tileMap.SetTile(new Vector3Int(-i-1, j + mapSize.y / 2 - 1, 0), wallTile);
+                tileMap.SetTile(new Vector3Int(i + 1, j, 0), wallTile);
+                tileMap.SetTile(new Vector3Int(-i - 1, j, 0), wallTile);
             }
 
             count++;
