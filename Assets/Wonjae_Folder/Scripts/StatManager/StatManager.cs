2c959525ae2e88e8517e492679dcac3ee627b192
21aje <diding0222@naver.com>
2023-09-05 14:58:31 +0900

    1

---
+++ Assets/Wonjae_Folder/Scripts/StatManager/StatManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
