a608b736ec8820aaf6f28cdb380b2ebb2e94d65a
cientodos <wjsrlqor777@gmail.com>
2023-09-06 09:10:32 +0900

    102

---
+++ Assets/102/Script/StunEntity.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunEntity : MonoBehaviour
+{
+    [SerializeField] public float Atk;
+    [SerializeField] public float Speed;
+}
--- Assets/102/Script/StunTower.cs
+++ Assets/102/Script/StunTower.cs
@@ -16,6 +16,7 @@ public class StunTower : SubTower
    
     #endregion
     public GameObject Stun;
+    public GameObject NonAutoStun;
     private void Start()
     {
      
@@ -34,12 +35,12 @@ public class StunTower : SubTower
             if (StunRestTime > StunRestCool)
             {
                
-                    StartCoroutine("StunAtk");
-                
+                    StartCoroutine("AutoStunAtk");
+                    StunAttack();
             }
             else 
             {
-                StopCoroutine("StunAtk");
+                StopCoroutine("AutoStunAtk");
         
             }
         }
@@ -50,15 +51,23 @@ public class StunTower : SubTower
     {
         StunRestTime += Time.deltaTime;
     }
-    
-  
-    IEnumerator StunAtk()
+
+    void StunAttack()
     {
-        
+        if (SkillTreeManager.Instance.isTech3 == false)
+        {
+            GameObject SA = Instantiate(NonAutoStun, StunPos.transform.position, StunPos.transform.rotation);
+            Destroy(SA, 5f);
+            StunRestTime = 0;
+        }
+    }
+    IEnumerator AutoStunAtk()
+    {
+        if (SkillTreeManager.Instance.isTech3 == true) { 
         GameObject StunAmmo = Instantiate(Stun, StunPos.transform.position, StunPos.transform.rotation);
         Destroy(StunAmmo, 5f);
         StunRestTime = 0;
-        yield return new WaitForSeconds(StunRestCool); 
-        
+        yield return new WaitForSeconds(StunRestCool);
+        }
     }                   
 }
