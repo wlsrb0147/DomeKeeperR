ca07cb70df300dc8e43281176378201fb825a0eb
wlsrb0147 <wlsrb0147@gmail.com>
2023-09-07 16:01:21 +0900

    1234

--- Assets/Dong/M_Script/M_Flyer/M_FlyerHit.cs
+++ Assets/Dong/M_Script/M_Flyer/M_FlyerHit.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
-
 public class M_FlyerHit : M_State
 {
     M_Flyer flyer;
--- Assets/Wonjae_Folder/Scripts/Player/WJ_PlayerDigState.cs
+++ Assets/Wonjae_Folder/Scripts/Player/WJ_PlayerDigState.cs
@@ -1,9 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.Tilemaps;
-using UnityEngine;
-
 public class WJ_PlayerDigState : WJ_PlayerGroundState
 {
     public WJ_PlayerDigState(WJ_Player _player, WJ_PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
@@ -21,7 +15,7 @@ public class WJ_PlayerDigState : WJ_PlayerGroundState
 
 
 
-       player.SetVelocity(xInput * player.Speed, rbody.velocity.y);
+        player.SetVelocity(xInput * player.Speed, rbody.velocity.y);
 
 
         /*     if (mousePosition.x > player.transform.position.x)
@@ -32,7 +26,7 @@ public class WJ_PlayerDigState : WJ_PlayerGroundState
              {
                  player.Flip();
              }*/
-    
+
 
 
     }
