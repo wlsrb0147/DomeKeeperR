3b1fd9f06b09b10f1a0ee1c670ed6428bfdbfc66
wlsrb0147 <wlsrb0147@gmail.com>
2023-09-11 17:31:15 +0900

    123456

--- Assets/SJH/Script/S_ScoreManager.cs
+++ Assets/SJH/Script/S_ScoreManager.cs
@@ -13,15 +13,24 @@ public class S_ScoreManager : MonoBehaviour
     [SerializeField] Text petBluejemScore;
     [SerializeField] Text petGreenjemScore;
 
+    public GameObject stash;
+
     void Update()
     {
-        redjemScore.text = S_GameManager.instance.player.redjemScore.ToString();
-        bluejemScore.text = S_GameManager.instance.player.bluejemScore.ToString();
-        greenjemScore.text = S_GameManager.instance.player.greenjemScore.ToString();
+        //redjemScore.text = S_GameManager.instance.player.redjemScore.ToString();
+        //bluejemScore.text = S_GameManager.instance.player.bluejemScore.ToString();
+        //greenjemScore.text = S_GameManager.instance.player.greenjemScore.ToString();
+
+        redjemScore.text = stash.GetComponent<S_JemstoneStash>().redjemScore.ToString();
+        bluejemScore.text = stash.GetComponent<S_JemstoneStash>().bluejemScore.ToString();
+        greenjemScore.text = stash.GetComponent<S_JemstoneStash>().greenjemScore.ToString();
 
         petRedjemScore.text = (S_GameManager.instance.pet.redjemScore+ S_GameManager.instance.pet2.redjemScore).ToString();
         petBluejemScore.text = (S_GameManager.instance.pet.bluejemScore + S_GameManager.instance.pet2.bluejemScore).ToString();
         petGreenjemScore.text = (S_GameManager.instance.pet.greenjemScore + S_GameManager.instance.pet2.greenjemScore).ToString();
+
+        Debug.Log(stash.GetComponent<S_JemstoneStash>().redjemScore);
+
 
     }
 }
